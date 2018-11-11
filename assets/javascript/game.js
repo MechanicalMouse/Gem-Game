a1ee397b586e16c3cf9a55da958f7df53e3035d2
MechanicalMouse <LauraG0@hotmail.com>
2018-11-11 12:54:21 -0700

    WIP3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,12 @@
 var counter = 0;
-var goal = ;
+var goal = 55;
 var numberOptions = [3, 5, 7, 10]
-var increment = numberOptions[Math.round(Math.random())];
 var gemArray = ["assets/images/green-gem.png","assets/images/blue-gem.png","assets/images/purple-gem.png","assets/images/pink-gem.png"]
 var wins = 0;
 var losses = 0;
 
-$("#goalNumber").text(goal);
 
+$("#goal-number").text(goal);
 
 for (var i = 0; i < numberOptions.length; i++) {
 
@@ -20,7 +19,8 @@ for (var i = 0; i < numberOptions.length; i++) {
     imageGem.attr("data-gemvalue", numberOptions[i]);
     
     $("#gems").append(imageGem);
-    }
+    
+}
     
 $(".gem-image").on("click", function() {
 
@@ -31,18 +31,17 @@ $(".gem-image").on("click", function() {
 
     alert("New score is: " + counter);
 
-    if (counter === goal) {
-        wins++;
-        document.getElementById("wins").innerHTML = "Wins: " + wins;
-        reset();
+if (counter === goal) {
+    alert("You win!")
+        
+    wins++;
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+    reset();
 
-        alert("You win!");
-
-    } else if (counter >= goal) {
-        losses++;
-    	document.getElementById("losses").innerHTML = "Losses: " + losses;
-        reset();
-
-        alert("Too high, you lose!");
-    }
-});
+} else if (counter >= goal) {
+    alert("Too high, you lose!");
+        
+    losses++;
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+    reset();
+}});
