8d74b88fc47c80f32d73922c582b9e6817ec7b4e
MechanicalMouse <LauraG0@hotmail.com>
2018-11-11 10:41:23 -0700

    WIP2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,48 @@
-var winningNum = 50;
+var counter = 0;
+var goal = ;
+var numberOptions = [3, 5, 7, 10]
+var increment = numberOptions[Math.round(Math.random())];
+var gemArray = ["assets/images/green-gem.png","assets/images/blue-gem.png","assets/images/purple-gem.png","assets/images/pink-gem.png"]
+var wins = 0;
+var losses = 0;
 
-$("#number-to-guess").text(winningNum);
+$("#goalNumber").text(goal);
 
-$("#green-gem").on("click", function() {
 
-    counter += 10;
+for (var i = 0; i < numberOptions.length; i++) {
 
-    alert("Your new score is: " + counter);
-
-});
-
-$("#blue-gem").on("click", function() {
-
-    counter += 10;
+    var imageGem = $("<img>");
     
-    alert("Your new score is: " + counter);
-
-});
-
-$("#purple-gem").on("click", function() {
-
-    counter += 10;
+    imageGem.addClass("gem-image");
     
-    alert("Your new score is: " + counter);
-
-});
-
-$("#pink-gem").on("click", function() {
-
-    counter += 10;
+    imageGem.attr("src", gemArray[i]);
     
-    if (counter === targetNumber) {
+    imageGem.attr("data-gemvalue", numberOptions[i]);
+    
+    $("#gems").append(imageGem);
+    }
+    
+$(".gem-image").on("click", function() {
+
+    var gemValue = ($(this).attr("data-gemvalue"));
+    gemValue = parseInt(gemValue);
+
+    counter += gemValue;
+
+    alert("New score is: " + counter);
+
+    if (counter === goal) {
+        wins++;
+        document.getElementById("wins").innerHTML = "Wins: " + wins;
+        reset();
 
         alert("You win!");
+
+    } else if (counter >= goal) {
+        losses++;
+    	document.getElementById("losses").innerHTML = "Losses: " + losses;
+        reset();
+
+        alert("Too high, you lose!");
     }
 });
-
